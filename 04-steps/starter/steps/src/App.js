672b4b1acccb8c83e75acd4536686677eea0bb8f
abdo-ss1 <abdo-ss1@hotmail.com>
2026-08-21 19:08:18 +0300

    finishing steps app

--- 04-steps/starter/steps/src/App.js
+++ 04-steps/starter/steps/src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const messages = [
+    "Learn React ⚛️",
+    "Apply for jobs 💼",
+    "Invest your new income 🤑",
+  ];
+
+  const [step, setStep] = useState(0);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handleNext() {
+    setStep((s) => (s < messages.length - 1 ? s + 1 : s));
+  }
+
+  function handlePrevious() {
+    setStep((s) => (s > 0 ? s - 1 : s));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        ×
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            {messages.map((_, i) => (
+              <div
+                key={i}
+                className={i <= step ? "active" : ""}
+                onClick={() => setStep(i)}
+              >
+                {i + 1}
+              </div>
+            ))}
+          </div>
+          <div className="message">{messages[step]}</div>
+          <div className="buttons">
+            <button onClick={handlePrevious} disabled={step === 0}>
+              ← Previous
+            </button>
+            <button
+              onClick={handleNext}
+              disabled={step === messages.length - 1}
+            >
+              Next →
+            </button>
+          </div>
+        </div>
+      )}
+      ;
+    </>
   );
 }
 
