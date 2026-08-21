e88aab88d5b6d65a4d221ee198ad5467ed7019da
abdo-ss1 <abdo-ss1@hotmail.com>
2026-08-21 20:46:02 +0300

    Editing DateCounter

--- 04.5-date-counter/date-counter/src/App.js
+++ 04.5-date-counter/date-counter/src/App.js
@@ -8,13 +8,22 @@ function App() {
   return (
     <div className="App">
       <div>
-        <button onClick={() => setStep(Math.max(1, step - 1))}>-</button>
-        <span>steps:{step}</span>
-        <button onClick={() => setStep(step + 1)}>+</button>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        <span>Step: {step}</span>
       </div>
       <div>
         <button onClick={() => setCount(count - step)}>-</button>
-        <span>count:{count}</span>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
         <button onClick={() => setCount(count + step)}>+</button>
       </div>
       <div>
@@ -29,6 +38,19 @@ function App() {
           ).toDateString()}
         </span>
       </div>
+      {count !== 0 || step !== 1 ? (
+        <div>
+          <button
+            className="btn btn-danger"
+            onClick={() => {
+              setCount(0);
+              setStep(1);
+            }}
+          >
+            Reset
+          </button>
+        </div>
+      ) : null}
     </div>
   );
 }
